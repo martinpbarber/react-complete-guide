fd81e125cc93ed6467ff55775a01bc99633035d0
Martin Barber <martinpbarber@gmail.com>
2023-08-23 20:08:31 -0400

    Alternate way to pass props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+function ExpenseItem({ title, amount, date }) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
